c2fa7f86bf6f35e23b46a9b46ba736dbfc545560
saif.alam <saif.alam@saif-su-pc.ZILLIOUS>
2022-08-05 01:05:34 +0530

    DB connection class added

--- src/main/java/com/tech/blogs/helper/DBConnection.java
+++ src/main/java/com/tech/blogs/helper/DBConnection.java
@@ -1,5 +1,49 @@
 package com.tech.blogs.helper;
 
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
 public class DBConnection {
+
+	private static Connection connection;
+
+	public static Connection getDBConnection() {
+
+		try {
+
+			FileInputStream fileInputStream = new FileInputStream(
+					"D:\\Study\\java\\NewIDE\\TechnicalBlogs\\src\\main\\java\\db.properties");
+
+			Properties props = new Properties();
+			props.load(fileInputStream);
+
+			// properties to connect with DB
+			String driver = props.getProperty("driver");
+			String url = props.getProperty("url");
+			String dbUserName = props.getProperty("user");
+			String dbPassword = props.getProperty("password");
+
+			if (connection == null) {
+				// load driver class
+
+				Class.forName(driver);
+
+				// established connection with DB
+				connection = DriverManager.getConnection(url, dbUserName, dbPassword);
+			 }
+			else {
+				System.out.println("connection already created");
+				return connection;
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		return connection;
+	}
 
 }
